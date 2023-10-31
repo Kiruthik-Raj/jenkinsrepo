3705857e6c5fe3864350e609a5305792060bc6dd
RAJ <RAJ@BEAST>
2023-10-31 21:02:49 +0530

    Second commit incurring change to the source folder

---
+++ mavenjenkins/src/main/java/mavenjenkins/changeclass.java
@@ -0,0 +1,9 @@
+package mavenjenkins;
+
+public class changeclass {
+	public static void main(String args[])
+	{
+		System.out.println("Change in code");
+		System.out.println("We push this into the repository to be detected by jenkins");
+	}
+}
